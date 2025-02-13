dc1c5d0a2182d2bdd4115f346c03c84cfeeab3dc
zadrom <zadrom02@gmail.com>
2025-02-13 13:31:55 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,15 +6,37 @@ import networkx as nx
 def nacrtaj_mrezu():
     g = nx.Graph()
     
-    # Čvorovi (trafostanice)
-    g.add_nodes_from(["TS1", "TS2", "TS01", "TS02", "TS03", "TS04", "TS05", "TS06", "TS07", "TS08", "TS09"])
-    
+   # Dodavanje trafostanica
+tstations = {
+    "TS1": "brown", "TS2": "darkblue", "TS01": "brown", "TS02": "brown", "TS03": "brown", "TS04": "brown", 
+    "TS05": "brown", "TS06": "darkblue", "TS07": "darkblue", "TS08": "darkblue", "TS09": "darkblue"
+}
     # Povezivanje dalekovodima
-    edges = [("TS1", "TS01"), ("TS01", "TS02"), ("TS02", "TS03"), ("TS03", "TS2"),
-             ("TS1", "TS04"), ("TS04", "TS05"), ("TS05", "TS06"), ("TS06", "TS2"),
-             ("TS2", "TS09"), ("TS09", "TS08"), ("TS08", "TS07")]
-    
-    g.add_edges_from(edges)
+    edges = {
+    ("TS1", "TS01"): "green", ("TS01", "TS02"): "green", ("TS02", "TS03"): "green", ("TS03", "TS2"): "lightblue", 
+    ("TS1", "TS04"): "green", ("TS04", "TS05"): "green", ("TS05", "TS06"): "lightblue", ("TS2", "TS06"): "green", 
+    ("TS1", "TS07"): "lightblue", ("TS2", "TS09"): "green", ("TS08", "TS09"): "green", ("TS07", "TS08"): "green", 
+    ("TS04", "TS02"): "lightblue", ("TS05", "TS09"): "lightblue"
+}
+edge_labels = {
+    ("TS1", "TS01"): "D1", ("TS01", "TS02"): "D2", ("TS02", "TS03"): "D3", ("TS03", "TS2"): "D4", 
+    ("TS1", "TS04"): "D5", ("TS04", "TS05"): "D6", ("TS05", "TS06"): "D7", ("TS06", "TS2"): "D8", 
+    ("TS1", "TS07"): "D9", ("TS2", "TS09"): "D12", ("TS09", "TS08"): "D11", ("TS08", "TS07"): "D10", 
+    ("TS04", "TS02"): "D13", ("TS05", "TS09"): "D14"
+}
+g.add_edges_from(edges.keys())
+
+# Osiguravanje da svi rubovi imaju definisane boje
+edge_colors = [edges.get(edge, "lightblue") for edge in g.edges]
+
+# Prilagođavanje pozicija kako bi se izbjeglo preklapanje vodova
+pos = {
+    "TS1": (-3, 2), "TS2": (3, 2),
+    "TS01": (-2, 1), "TS02": (0, 1), "TS03": (2, 1),
+    "TS04": (-2, 0), "TS05": (0, 0), "TS06": (2, 0),
+    "TS07": (-2, -1), "TS08": (0, -1), "TS09": (2, -1)
+}  
+    g.add_edges_from(tstations.keys())
     
     # Crtanje mreže
     pos = nx.spring_layout(g)
